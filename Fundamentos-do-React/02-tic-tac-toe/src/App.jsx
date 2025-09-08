70756c13a76ada84efa82569683641a9f8c9c7ab
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-09-08 16:09:13 -0300

    fix: ajustado case do GameBoard

--- Fundamentos-do-React/02-tic-tac-toe/src/App.jsx
+++ Fundamentos-do-React/02-tic-tac-toe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import Player from "./components/Player"
-import GameBoard from "./components/Gameboard"
+import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import {WINNING_COMBINATIONS} from "./winning-combinations"
 import GameOver from "./GameOver"
